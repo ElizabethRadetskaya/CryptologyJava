5cb09073b235cf782a1f6442bb8c9414b55bc0fa
Elizabeth Radetskaya <elizavetaradetskaa@gmail.com>
2024-06-24 07:31:54 +0300

    Add new file

--- src/com/java/radetskaya/crypto/Decrypt.java
+++ src/com/java/radetskaya/crypto/Decrypt.java
@@ -1,5 +1,12 @@
 package com.java.radetskaya.crypto;
 
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Decrypt {
@@ -10,35 +17,72 @@ public class Decrypt {
         char[] input;
         int key;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Введіть текст для розшифрування: ");
+        System.out.println("Введіть шлях до файлу, де є текст для розшифрування: ");
+
         String ip = sc.nextLine();
-        input = ip.toCharArray();
+
+
+
+        List<String> arrayList = new ArrayList<>();
+        String string = null;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(ip))) {
+
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                StringBuilder stringBuilder = new StringBuilder(line);
+                StringBuilder stringBuilderCopy = new StringBuilder(line);
+                String stringReverse = stringBuilder.toString();
+                string = stringBuilderCopy.toString();
+                if (stringReverse.equals(string)) {
+                    arrayList.add(string);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        input = string.toCharArray();
+
+        System.out.println("Строки в массив символов: " + Arrays.toString(input));
+
+
+
         System.out.println("Введіть ключ: ");
         key = sc.nextInt();
         for (int i = 0; i < input.length; i++)
         {
             if(input[i]==' ')
                 continue;
-            else
-            {
-                if(input[i]>= 'А' && input[i]<='Я')
-                {
-                    input[i]=(char) (input[i]-key);
-                    if(input[i]<'А')
-                    {
-                        input[i] = (char) (input[i]+32);
+            else {
+                if (input[i] >= 'А' && input[i] <= 'Я') {
+                    input[i] = (char) (input[i] - key);
+                    if (input[i] < 'А') {
+                        input[i] = (char) (input[i] + 32);
                     }
-                }
-                else
-                {
-                    input[i]=(char) (input[i]-key);
-                    if (input[i]<'а')
-                    {
-                        input[i] = (char) (input[i]+32);
+                } else {
+                    input[i] = (char) (input[i] - key);
+                    if (input[i] < 'а') {
+                        input[i] = (char) (input[i] + 32);
                     }
                 }
             }
+
+            try {
+                FileOutputStream fos = new FileOutputStream("Text3.txt");
+                String text = String.valueOf(input);
+                byte[] textBytes = text.getBytes();
+                fos.write(textBytes);
+                fos.close();
+            } catch (IOException e) {
+                System.out.println("Ошибка записи в файл: " + e.getMessage());
+            }
+
         }
         System.out.println("Розшифрований рядок: "+String.valueOf(input)+ "\n");
+
     }
 }
+
+
+
--- src/com/java/radetskaya/crypto/Encrypt.java
+++ src/com/java/radetskaya/crypto/Encrypt.java
@@ -12,71 +12,79 @@ import java.util.Scanner;
 public class Encrypt {
 
 
-        public static void encrypt() {
-            char[] input;
-            int key;
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Введіть текст для шифрування: ");
+    public static void encrypt() {
+        char[] input;
+        int key;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введіть шлях до файлу, де є текст для зашифрування: ");
+
+        String ip = sc.nextLine();
 
 
-            List<String> arrayList = new ArrayList<>();
-            String string = null;
-            try (BufferedReader bufferedReader = new BufferedReader(new FileReader("Text.txt"))) {
+
+        List<String> arrayList = new ArrayList<>();
+        String string = null;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(ip))) {
 
 
-                String line;
-                while ((line = bufferedReader.readLine()) != null) {
-                    StringBuilder stringBuilder = new StringBuilder(line);
-                    StringBuilder stringBuilderCopy = new StringBuilder(line);
-                    String stringReverse = stringBuilder.toString();
-                    string = stringBuilderCopy.toString();
-                    if (stringReverse.equals(string)) {
-                        arrayList.add(string);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                StringBuilder stringBuilder = new StringBuilder(line);
+                StringBuilder stringBuilderCopy = new StringBuilder(line);
+                String stringReverse = stringBuilder.toString();
+                string = stringBuilderCopy.toString();
+                if (stringReverse.equals(string)) {
+                    arrayList.add(string);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        input = string.toCharArray();
+
+        System.out.println("Строки в массив символов: " + Arrays.toString(input));
+
+
+        System.out.println("Введіть ключ для шифрування: ");
+        key = sc.nextInt();
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] == ' ')
+                continue;
+            else {
+                if (input[i] >= 'А' && input[i] <= 'Я') {
+                    input[i] = (char) (key + input[i]);
+                    if (input[i] > 'Я') {
+                        input[i] = (char) (input[i] - 32);
+                    }
+                } else {
+                    input[i] = (char) (key + input[i]);
+                    if (input[i] > 'я') {
+                        input[i] = (char) (input[i] - 32);
                     }
                 }
+            }
+
+
+            try {
+                FileOutputStream fos = new FileOutputStream("Text4.txt");
+                String text = String.valueOf(input);
+                byte[] textBytes = text.getBytes();
+                fos.write(textBytes);
+                fos.close();
             } catch (IOException e) {
-                e.printStackTrace();
+                System.out.println("Ошибка записи в файл: " + e.getMessage());
             }
 
-            input = string.toCharArray();
 
-            System.out.println("Строку в массив символов: " + Arrays.toString(input));
-
-
-            System.out.println("Введіть ключ для шифрування: ");
-            key = sc.nextInt();
-            for (int i = 0; i < input.length; i++) {
-                if (input[i] == ' ')
-                    continue;
-                else {
-                    if (input[i] >= 'А' && input[i] <= 'Я') {
-                        input[i] = (char) (key + input[i]);
-                        if (input[i] > 'Я') {
-                            input[i] = (char) (input[i] - 32);
-                        }
-                    } else {
-                        input[i] = (char) (key + input[i]);
-                        if (input[i] > 'я') {
-                            input[i] = (char) (input[i] - 32);
-                        }
-                    }
-
-                }
-
-                try {
-                    FileOutputStream fos = new FileOutputStream("Text2.txt");
-                    String text = String.valueOf(input);
-                    byte[] textBytes = text.getBytes();
-                    fos.write(textBytes);
-                    fos.close();
-                } catch (IOException e) {
-                    System.out.println("Ошибка записи в файл: " + e.getMessage());
-                }
-
-            }
 
         }
+
+
+
     }
+
+}
 
 
 
